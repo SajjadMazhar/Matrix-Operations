936a62528b2c8b9f0d57ee3debf49e7d24415f0f
Sajjad <sajjadthedevil123@gmail.com>
2021-09-21 12:24:32 +0530

    basic operation class

--- matrixOperations.py
+++ matrixOperations.py
@@ -1,41 +1,64 @@
-def addition(a, b):
-    result = []
-    for i in range(len(a)):
-        row = []
-        for j in range(len(a[0])):
-            c = a[i][j] + b[i][j]
-            row.append(c)
-        result.append(row)
-    return result
+class MatrixOperations:
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+
+	def constructMatrix(self):
+		print("Enter elements one by one row wise: ")
+		resultMat =[]
+		for i in range(self.row):
+			rowElm = []
+			for j in range(self.col):
+				print(f"Enter the element ({i+1}, {j+1}) = ",end="")
+				element = int(input())
+				rowElm.append(element)
+			resultMat.append(rowElm)
+		return resultMat
+
+	def sumMatrix(self, matrix_A, matrix_B):
+		resultSum = []
+		for i in range(len(matrix_A)):
+			rowElm = []
+			for j in range(len(matrix_B)):
+				resutlElm = matrix_A[i][j] + matrix_B[i][j]
+				rowElm.append(resutlElm)				
+			resultSum.append(rowElm)
+		return resultSum
+
+	def subtMatrix(self, matrix_A, matrix_B):
+		resultSubt = []
+		for i in range(len(matrix_A)):
+			rowElm = []
+			for j in range(len(matrix_B)):
+				resutlElm = matrix_A[i][j] - matrix_B[i][j]
+				rowElm.append(resutlElm)				
+			resultSubt.append(rowElm)
+		return resultSubt
+
+	def multMatrix(self, matrix_A, matrix_B):
+		resultMult = []
+		for i in range(len(matrix_A)):
+			rowElm = []
+			for j in range(len(matrix_B[0])):
+				sop = 0
+				for k in range(len(matrix_A[0])):
+					sop = sop + matrix_A[i][k] + matrix_B[k][j]
+				rowElm.append(sop)
+			resultMult.append(rowElm)
+		return resultMult
 
 
-def matrix_multi(a, b):
-    mul = []
-    for i in range(len(a)):
-        row = []
-        for j in range(len(b[0])):
-            sop = 0
-            for k in range(len(a[0])):
-                sop = sop + a[i][k] * b[k][j]
-            row.append(sop)
-        mul.append(row)
-    return mul
+if __name__ == "__main__":
 
-def mkMatrix(m, n): # m, n are the parameters for the index(order) of the matrix
-    mat = []
-    for i in range(m):
-        row = []
-        for j in range(n):
-            print(f"Enter the element ({i+1}, {j+1}) = ",end="")
-            element = int(input())
-            row.append(element)
-        mat.append(row)
-    return mat
+	m, n = 2, 2 # order of the matrix
+	operation = MatrixOperations(m, n)
 
-        
-
-print("Enter the order of the matrix:")
-m, n = int(input()), int(input())
-print(mkMatrix(m,n))
-
-
+	# creating matrices
+	matrix1 = operation.constructMatrix()
+	matrix2 = operation.constructMatrix()
+	
+	# basic operations
+	addition = operation.sumMatrix(matrix1, matrix2)
+	subtraction = operation.subtMatrix(matrix1, matrix2)
+	multiplication = operation.multMatrix(matrix1, matrix2)
+	print(f"\n\taddition: {addition}\n\tsubtraction: {subtraction}\n\tmultiplication: {multiplication}")
